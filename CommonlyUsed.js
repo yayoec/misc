cdb672fdbc436e094ebcab8a9d13e47c82562d6b
Jey <yayoec@gmail.com>
2012-10-15 18:50:05 +0800

    event
    事件处理常用

--- CommonlyUsed.js
+++ CommonlyUsed.js
@@ -83,7 +83,106 @@ Util = {
 		} else {
 			return !!(parentNode.compareDocumentPosition(childNode) & 16);
 		}
-	 }	 
+	 },	 
 	
-	//
+	//事件
+	addHandler : function (ele, type, handler) {
+		if (ele.addEventListener) {
+			ele.addEventListener(type, handler, false);
+		}else if(ele.attachEvent){
+			ele.attachEvent('on'+type, handler);
+		}else {
+			ele['on'+type] = handler;
+		}
+	},
+	removeHandler : function (ele, type, handler) {
+		if (ele.removeEventListener) {
+			ele.removeEventListener(type, handler, false);
+		}else if (ele.detachEvent) {
+			ele.detachEvent('on'+type, handler);
+		}else{
+			ele['on'+type] = null;
+		}
+	},
+	getEvent : function (e) {
+		return e || window.event;
+	},
+	getTarget : function (event) {
+		return event.target || event.srcElement;
+	},
+	preventDefault : function (event) {
+		if (event.preventDefault) {
+			event.preventDefault();
+		}else {
+			event.returnValue = false;
+		}
+	},
+	stopPropagation : function (event) {
+		if (event.stopPropagation) {
+			event.stopPropagation();
+		}else {
+			event.cancelBubble = true;
+		}
+	},
+	getRelatedTarget : function (event) {
+		if (event.relatedTarget) {
+			return event.relatedTarget;
+		}else if (event.toElement) {
+			return event.toElement;
+		}else if (event.fromElement) {
+			return event.fromElement;
+		}else {
+			return null;
+		}
+	},
+	getButton : function (event) {
+		if (document.implementation.hasFeature("MouseEvents", "2.0")) {
+			return event.button;
+		}else {
+			switch (event.button) {
+				case 0:
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+					return 0;
+				case 2:
+				case 6:
+					return 2;
+				case 4:
+					return 1;
+				
+			
+			}
+		}
+	},
+	//可以获得焦点的元素才可以触发keypress 可编辑区域可触发textInput事件 字符编码
+	getCharCode : function (event) {
+		if (typeof event.charCode == 'number') {
+			return event.charCode;
+		}else {
+			return event.keyCode;
+		}
+	},
+	//禁用鼠标右键的默认上下文事件
+	//Util.addHandler(document,'contextmenu',function(e){});
+	//页面卸载事件 beforeunload事件 所有浏览器都支持
+	//mousewheel 鼠标滚动事件
+
+	//剪切板事件 opera不支持
+	getClipboardText : function (event) {
+		var clipBoardData = (event.clipboardData || window.clipboardData);
+		return clipboardData.getData('text');
+	},
+	setClipboardText : function (event, value) {
+		if (event.clipboardData) {
+			return event.clipboardData.setData('text/plain', value);
+		}else if (window.clipboardData) {
+			return window.clipboardData.setData('text', value);
+		}
+	},
+	
+	
+	
+	
 }
