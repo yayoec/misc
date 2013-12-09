1e360971eaa526fdd6ac20ccb51424be0af70c9c
yayoec <yayoec@gmail.com>
2013-12-09 18:18:22 +0800

    添加图片旋转

--- js/CommonlyUsed.js
+++ js/CommonlyUsed.js
@@ -334,3 +334,25 @@ DragDrop.addHandler("dragend", function(event){
 	status.innerHTML += "<br />Dropped " + event.target.id + " at (" + event.x + "," + event.y + ")";
 });
 */
+
+var userAgent = navigator.userAgent,
+	isIE = /msie/i.test(userAgent) && !window.opera,
+	isWebKit = /webkit/i.test(userAgent),
+	isFirefox = /firefox/i.test(userAgent);
+function rotate(target, degree) {
+    if (isWebKit) {
+        target.style.webkitTransform = "rotate(" + degree + "deg)";
+    } else if (isFirefox) {
+        target.style.MozTransform = "rotate(" + degree + "deg)";
+    } else if (isIE) {
+        //chessDiv.style.filter = "progid:DXImageTransform.Microsoft.BasicImage(rotation=" + degree + ")";
+        degree = degree / 180 * Math.PI;
+        var sinDeg = Math.sin(degree);
+        var cosDeg = Math.cos(degree);
+         
+        target.style.filter = "progid:DXImageTransform.Microsoft.Matrix(" +
+                "M11=" + cosDeg + ",M12=" + (-sinDeg) + ",M21=" + sinDeg + ",M22=" + cosDeg + ",SizingMethod='auto expand')";
+    } else {
+        target.style.transform = "rotate(" + degree + "deg)";
+    }
+}
